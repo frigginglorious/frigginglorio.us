2463de438047da974d1e5480e63d2bdd129a8dd0
frigginglorious <frigginglorious@gmail.com>
2017-07-29 17:31:24 -0700

    static content rewrite

--- homeContent.php
+++ homeContent.php
@@ -3,14 +3,15 @@
 </h2>
 	<br>
 	<p>
-		I am currently seeking challenging, gainful employment. If you're interested in hiring a skilled, motivated, and all-around amazing individual like myself, you can view my official <a href="fil/DanielKraft_Resume.pdf" target="_blank">Resume</a>, but I would rather you take a look at the projects and things available on this site. It is a collection of passions and projects, and is a <a href="?postID=33">labor of love in itself</a>.</p>
+		I am currently seeking challenging, gainful employment. If you're interested in hiring a skilled, motivated, and all-around amazing individual like myself, you can view my official <a href="fil/DanielKraft_Resume.pdf" target="_blank">Resume</a>, but I would rather you take a look at the passions projects available on this site: a <a href="?postID=33">labor of love in itself</a>.</p>
 	<p>
-		My dream is to make livable residual income from monetized passion projects, allowing me the freedom to help fix the things I see wrong with the world. But right now, I'd love to lend my time to help your organization deliver something awesome in exchange for pay and some PTO.
-	<p>
-		The Job I'm currently seeking will require my passion for people as well as creative uses of open source technology; it will deliver new challenges every day, push my abilities to their furthest extent, and allow room to grow professionally. If you think you can help me find this job, please email me at <a href="mailto:kraftd@live.com">kraftd@live.com</a>.
+		My dream job is <strong>Developer Evangelist</strong>: creatively building and demonstrating technology. The Job I'm currently seeking will require my passion for people as well as creative uses of open source technology; it will deliver new challenges every day, push my abilities to their furthest extent, and allow room to grow professionally.
 	</p>
 	<p>
-		Though I currently dwell in Seattle, my primary residence will always be the internet.
+		If you think you can help me find this job, please email me at <a href="mailto:kraftd@live.com">kraftd@live.com</a>.
+	</p>
+	<p>
+		Though I currently dwell in the Capitol Hill neighborhood of Seattle, my primary residence will always be the internet.
 	</p>
 	<!-- <p>
 		If you are looking to contact me securely at my personal gmail address, my PGP public key is <a href="fil/pgpkey.txt" target="_blank">available here</a>
