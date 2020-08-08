5cc61708f9b3ce2e4426323cf19934b46d5e9673
dkraft <dkraft@camsbycbs.com>
2020-08-07 19:08:28 -0500

    add example config file

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+// modify values and save as config.php
+return array(
+    'host' => 'localhost',
+    'username' => 'imAnAdmin',
+    'password' => 'letMeIn',
+    'database' => 'sweetPortfolioDb',
+);
+?>
